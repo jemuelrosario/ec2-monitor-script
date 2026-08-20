c84431af4a2ec149d3b810737e238807fb601da1
jemuelrosario <jemuel.rosario@upr.edu>
2026-08-20 01:00:55 -0400

    Add core logic: calculate running time, check alert threshold and fetchEC2 instances

---
+++ ec2_monitor.py
@@ -0,0 +1,35 @@
+import boto3
+from datetime import datetime, timezone, timedelta
+from instance import Instance
+
+umbral = timedelta(hours=1)
+
+def obtain_instances():
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances()
+
+    instances = []
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instances.append(Instance(instance['InstanceId'], instance['State']['Name'], instance['LaunchTime']))
+
+    return instances
+
+
+def running_time(instance):
+    actual_date_UTC = datetime.now(timezone.utc)
+    run_time = actual_date_UTC - instance.launch_time  
+    return run_time
+
+def check_running_time(instance, threshold):
+    run_time = running_time(instance)
+    if instance.state == 'running' and run_time  >= threshold:
+        return 'This instance has surpassed the maximium running time'
+    elif instance.state == 'running' and run_time < threshold:
+        return 'Valid running time of the instance'
+    else:
+        return 'N/A'
+
+
+
---
+++ instance.py
@@ -0,0 +1,5 @@
+class Instance:
+    def __init__(self, instance_id, state, launch_time):
+        self.instance_id = instance_id
+        self.state = state
+        self.launch_time = launch_time
